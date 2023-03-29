bdfb6be67656a8dab20a328f9e766842cd9e52e4
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2023-03-29 17:26:52 +0100

    inti front end

---
+++ SkyRadio.Client.Identity/Services/LoginService.cs
@@ -0,0 +1,52 @@
+﻿using SkyRadio.Client.Identity.Services.Interfaces;
+using SkyRadio.Domain.Commons;
+using SkyRadio.Domain.Entities;
+using System.Dynamic;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+
+namespace SkyRadio.Client.Identity.Services
+{
+    public class LoginService : ILoginService
+    {
+        private readonly HttpClient _httpClient;
+
+        public LoginService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task LoginAsync(string username, string password)
+        {
+            try
+            {
+                var p = new Playload
+                {
+                    Buffer = new byte[1024],
+                    BuffuredDataLength = 1024,
+                    ChannelId = Guid.NewGuid().ToString(),
+                };
+                var result = await _httpClient.PostAsJsonAsync("/identiy/login", p);
+                var get = await _httpClient.GetFromJsonAsync<Response<object>>("");
+                if (result.IsSuccessStatusCode)
+                {
+                    var  json = await result.Content.ReadAsStringAsync();
+                    var response = JsonSerializer.Deserialize<Response<object>>(json);
+                    
+                }
+                else
+                {
+                    if(result.StatusCode == System.Net.HttpStatusCode.BadRequest)
+                    {
+
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Write(e);
+            }
+        }
+    }
+}
